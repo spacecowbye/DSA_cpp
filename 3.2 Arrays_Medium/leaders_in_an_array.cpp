979c0492f7d058f82ac2c3d0b7f8caf4b8d456e2
prophecyprogrammer <vatsal.shah2304@gmail.com>
2023-07-06 17:55:42 +0530

    Leaders in the array problem

---
+++ 3.2 Arrays_Medium/leaders_in_an_array.cpp
@@ -0,0 +1,2 @@
+//BRUTE FORCE SOLUTION
+//for every single element in nums, do a linear search on the right side of the array and check if any element on the right is bigger than our current element
