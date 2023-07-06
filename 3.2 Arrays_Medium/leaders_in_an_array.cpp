1ad7ba5459692a3fa7bed1baf1b0603198e4d995
prophecyprogrammer <vatsal.shah2304@gmail.com>
2023-07-06 21:04:35 +0530

    optimised code

--- 3.2 Arrays_Medium/leaders_in_an_array.cpp
+++ 3.2 Arrays_Medium/leaders_in_an_array.cpp
@@ -1,2 +1,23 @@
 //BRUTE FORCE SOLUTION
 //for every single element in nums, do a linear search on the right side of the array and check if any element on the right is bigger than our current element
+//optimal solution
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int arr[], int n){
+         vector<int> l;
+        int max1 = arr[n - 1];
+        l.push_back(max1);
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= max1) {
+                l.push_back(arr[i]);
+                max1 = arr[i];
+            }
+        }
+        reverse(l.begin(), l.end());
+        return l;
+        
+    }
+};
+
+
